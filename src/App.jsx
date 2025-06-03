4d2c5be85d2e3574354798793995652239b6b00b
Anil Kumar <anil.kumar@tagbin.in>
2025-06-03 21:09:25 +0530

    console

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import GamePlay from "./components/GamePlay";
 
 function App() {
   const [isGameStarted, setIsGameStarted] = useState(false);
-  console.log("-----------------------------", isGameStarted);
+  console.log("This is for the testing lets see");
   const toggleGamePlay = () => {
     setIsGameStarted((prev) => !prev);
   };
