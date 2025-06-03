c7e4c8ba01a36bf025dc258d7a7d12bef7980b63
Anil Kumar <anil.kumar@tagbin.in>
2025-06-03 21:46:34 +0530

    this is for the testing

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import GamePlay from "./components/GamePlay";
 function App() {
   const [isGameStarted, setIsGameStarted] = useState(false);
   console.log("This is for the testing lets see");
+  console.log("I did some testing lests see it will merge or not");
   const toggleGamePlay = () => {
     setIsGameStarted((prev) => !prev);
   };
