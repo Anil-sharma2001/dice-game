d6c40a02cbb9e33b11122b87a52d5d6f401125d6
Anil Kumar <anil.kumar@tagbin.in>
2025-06-03 21:24:44 +0530

    this is the =cokke

--- src/Test.tsx
+++ src/Test.tsx
@@ -2,16 +2,4 @@ import { useState } from "react";
 import StartGame from "./components/StartGame";
 import GamePlay from "./components/GamePlay";
 
-function App() {
-  const [isGameStarted, setIsGameStarted] = useState(false);
-  console.log("This is for the testing lets see");
-  const toggleGamePlay = () => {
-    setIsGameStarted((prev) => !prev);
-  };
-
-  return (
-    <>{isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />}</>
-  );
-}
-
 export default App;
