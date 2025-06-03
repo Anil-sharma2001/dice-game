7bee525a3c4c38cee4eed6ebbfdd6910fd4802ac
Anil Kumar <anil.kumar@tagbin.in>
2025-06-03 21:37:04 +0530

    Revert "this is the =cokke"
    This reverts commit d6c40a02cbb9e33b11122b87a52d5d6f401125d6.

--- src/Test.tsx
+++ src/Test.tsx
@@ -2,4 +2,16 @@ import { useState } from "react";
 import StartGame from "./components/StartGame";
 import GamePlay from "./components/GamePlay";
 
+function App() {
+  const [isGameStarted, setIsGameStarted] = useState(false);
+  console.log("This is for the testing lets see");
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev);
+  };
+
+  return (
+    <>{isGameStarted ? <GamePlay /> : <StartGame toggle={toggleGamePlay} />}</>
+  );
+}
+
 export default App;
